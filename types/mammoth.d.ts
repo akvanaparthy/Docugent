ae7c823fd303030895f387f8baab3dc8c7ec8c75
akvanaparthy <vanaparthyakshay@gmail.com>
2025-09-18 16:21:58 -0400

    Add type declarations for pdf-parse and mammoth modules

---
+++ types/mammoth.d.ts
@@ -0,0 +1,29 @@
+declare module "mammoth" {
+  interface MammothResult {
+    value: string;
+    messages: any[];
+  }
+
+  interface MammothOptions {
+    styleMap?: string[];
+    includeEmbeddedStyleMap?: boolean;
+    includeDefaultStyleMap?: boolean;
+    convertImage?: any;
+    ignoreEmptyParagraphs?: boolean;
+    idPrefix?: string;
+  }
+
+  function convertToHtml(
+    buffer: Buffer,
+    options?: MammothOptions
+  ): Promise<MammothResult>;
+  function extractRawText(
+    buffer: Buffer,
+    options?: MammothOptions
+  ): Promise<MammothResult>;
+
+  export = {
+    convertToHtml,
+    extractRawText,
+  };
+}
